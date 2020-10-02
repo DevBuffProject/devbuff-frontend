7ba99d6c0a316012bfaa44591de158d74eeecd11
Alexander Macintosh <katkov.alexander@yahoo.com>
2020-10-02 21:58:36 +0300

    Default export localizations

--- modules/i18n/index.js
+++ modules/i18n/index.js
@@ -2,7 +2,7 @@ import {languagesLocalization} from './common/languagesLocalization'
 import {englishLocalization} from './en'
 import {russianLocalization} from './ru'
 
-export let localeConfiguration = {
+export default {
   en: {...englishLocalization, ...languagesLocalization},
   ru: {...russianLocalization, ...languagesLocalization},
 };
