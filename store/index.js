5422373e24495134bd151af412c582082e7b6943
Alexander Katkov <7architect@users.noreply.github.com>
2020-12-02 10:44:18 +0300

    debug

--- store/index.js
+++ store/index.js
@@ -9,6 +9,7 @@ export const mutations = {
 export const actions = {
   async getHealth({ commit, state }) {
     const { STATUSPAGE_BASE_URL } = this.$config
+    console.log('[ FETCH STATUS ]: fetching status');
     const { data } = await this.$axios.get('https://6yzwffsm5625.statuspage.io/api/v2/status.json')
 
     // commit('setHealth', data, { root: true })
