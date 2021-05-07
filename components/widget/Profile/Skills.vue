5a77e304d96d4da2ca36a221a3b175b1550e7102
Alexander Chapchuk <BigTows@yandex.ru>
2021-05-07 16:50:38 +0300

    Update Skills.vue

--- components/widget/Profile/Skills.vue
+++ components/widget/Profile/Skills.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="">
     <div class="">
+      {{ skills }}
       <swiper
         ref="swiperComponent"
         :options="{
           autoHeight: true,
           calculateHeight: true,
           lazy: true,
-          simulateTouch: true,
+          simulateTouch: false,
         }"
         :auto-update="true"
         style="height: auto; margin-left: 0"
@@ -15,22 +16,28 @@
         <swiper-slide>
           <atomic-list class="divide-y divide-gray-200">
             <atomic-list-item
-              v-for="skill of user.skills"
+              v-for="skill of skills"
               :key="skill.name"
               :class="[
                 'flex justify-between items-center group cursor-pointer',
               ]"
             >
               <label class="flex items-center w-full" @click.stop>
-                <input type="checkbox" class="mr-2" />
+                <input
+                  :id="'language' + skill.name"
+                  v-model="skill.checked"
+                  type="checkbox"
+                  class="mr-2"
+                />
                 <div>{{ skill.name }}</div>
               </label>
               <div
+                v-if="skill.specializations.length"
                 :class="[
                   'px-4 py-1 transition-colors rounded',
                   'group-hover:bg-primary group-hover:bg-opacity-10 active:text-primary',
                 ]"
-                @click="nextSlide"
+                @click="nextSlide(null, skill.specializations)"
               >
                 <svg-icon name="outline/cheveron-right" />
               </div>
@@ -39,42 +46,75 @@
         </swiper-slide>
 
         <swiper-slide>
-          <atomic-list-item
-            class="flex items-center group cursor-pointer"
-            @click="prevSlide"
-          >
+          <atomic-list class="divide-y divide-gray-200">
             <div
-              class="px-2 py-1 mr-2 transition-colors rounded group-hover:bg-primary group-hover:bg-opacity-10"
+              :class="[
+                'px-4 py-1 transition-colors rounded',
+                'group-hover:bg-primary group-hover:bg-opacity-10 active:text-primary',
+              ]"
+              @click="prevSlide()"
             >
               <svg-icon name="outline/cheveron-left" />
             </div>
-            <div>Назад</div>
-          </atomic-list-item>
-          <div class="p-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias
-            aspernatur aut deserunt doloribus eaque eius excepturi fugiat, hic
-            in iure, laudantium, magni nam non quod repellat repellendus ut
-            veniam.
-          </div>
+            <atomic-list-item
+              v-for="specialist of specialists"
+              :key="specialist.name"
+              :class="[
+                'flex justify-between items-center group cursor-pointer',
+              ]"
+            >
+              <label class="flex items-center w-full" @click.stop>
+                <input
+                  :id="'specialist' + specialist.name"
+                  v-model="specialist.checked"
+                  type="checkbox"
+                  class="mr-2"
+                />
+                <div>{{ specialist.name }}</div>
+              </label>
+              <div
+                v-if="specialist.frameworks.length"
+                :class="[
+                  'px-4 py-1 transition-colors rounded',
+                  'group-hover:bg-primary group-hover:bg-opacity-10 active:text-primary',
+                ]"
+                @click="nextSlide(specialist, specialist.frameworks)"
+              >
+                <svg-icon name="outline/cheveron-right" />
+              </div>
+            </atomic-list-item>
+          </atomic-list>
         </swiper-slide>
         <swiper-slide>
-          <atomic-list-item
-            class="flex items-center group cursor-pointer"
-            @click="toSlide(0)"
-          >
+          <atomic-list class="divide-y divide-gray-200">
             <div
-              class="px-2 py-1 mr-2 transition-colors rounded group-hover:bg-primary group-hover:bg-opacity-10"
+              :class="[
+                'px-4 py-1 transition-colors rounded',
+                'group-hover:bg-primary group-hover:bg-opacity-10 active:text-primary',
+              ]"
+              @click="prevSlide()"
             >
               <svg-icon name="outline/cheveron-left" />
             </div>
-            <div>Назад</div>
-          </atomic-list-item>
-          <div class="p-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias
-            aspernatur aut deserunt doloribus eaque eius excepturi fugiat, hic
-            in iure, laudantium, magni nam non quod repellat repellendus ut
-            veniam.
-          </div>
+            <atomic-list-item
+              v-for="framework of frameworks"
+              :key="framework.name"
+              :class="[
+                'flex justify-between items-center group cursor-pointer',
+              ]"
+            >
+              <label class="flex items-center w-full" @click.stop>
+                <input
+                  :id="'specialist' + framework.name"
+                  v-model="framework.checked"
+                  type="checkbox"
+                  class="mr-2"
+                  @change="onChangeSkill(framework, specialists)"
+                />
+                <div>{{ framework.name }}</div>
+              </label>
+            </atomic-list-item>
+          </atomic-list>
         </swiper-slide>
       </swiper>
     </div>
@@ -82,7 +122,13 @@
 </template>
 
 <script>
-import { computed, defineComponent, ref } from '@nuxtjs/composition-api'
+import {
+  useStore,
+  computed,
+  defineComponent,
+  ref,
+  reactive,
+} from '@nuxtjs/composition-api'
 import { useUser } from '@/composes'
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 
@@ -95,16 +141,107 @@ export default defineComponent({
     SwiperSlide,
   },
   setup() {
+    const store = useStore()
     const { user } = useUser()
     const swiperComponent = ref(null)
     const swiper = computed(() => swiperComponent.value?.$swiper)
-    const nextSlide = () => swiper.value.slideNext()
-    const prevSlide = () => swiper.value.slidePrev()
     const toSlide = (index) => swiper.value.slideTo(index)
+    const allSkills = store.getters['skills/skills']
+
+    const userSkill = user.value.skills
+
+    for (const language of allSkills) {
+      const indexUserLanguage = findName(userSkill, language.name)
+
+      if (indexUserLanguage === -1) {
+        continue
+      }
+      language.checked = true
+
+      const userSpecialization = userSkill[indexUserLanguage].specializations
+
+      for (const specialization of language.specializations) {
+        const indexUserSpecialization = findName(
+          userSpecialization,
+          specialization.name
+        )
+        if (indexUserSpecialization === -1) {
+          continue
+        }
+        specialization.checked = true
+
+        const userTechnologies =
+          userSpecialization[indexUserSpecialization].frameworks
+
+        for (const technology of specialization.frameworks) {
+          const indexUserTechnology = findName(
+            userTechnologies,
+            technology.name
+          )
+          if (indexUserTechnology === -1) {
+            continue
+          }
+          technology.checked = true
+        }
+      }
+    }
+
+    function findName(array, value) {
+      return array.findIndex((data) => {
+        return data.name === value
+      })
+    }
+
+    const specialists = ref([])
+    const frameworks = ref([])
+
+    let context
+    function nextSlide(rootData, currentData) {
+      context = rootData
+      if (specialists.value.length === 0) {
+        // Current language slide
+        specialists.value = currentData
+        swiper.value.slideNext()
+      } else if (frameworks.value.length === 0) {
+        // Current specialist slide
+        frameworks.value = currentData
+        swiper.value.slideNext()
+      }
+    }
+
+    function prevSlide() {
+      if (frameworks.value.length !== 0) {
+        frameworks.value = []
+        swiper.value.slidePrev()
+      } else if (specialists.value.length !== 0) {
+        specialists.value = []
+        swiper.value.slidePrev()
+      }
+    }
+
+    const skills = reactive(JSON.parse(JSON.stringify(allSkills)))
+
+    function onChangeSkill(target, prevTarget) {
+      console.log('Data: ')
+      console.log(target.checked)
+      console.log(prevTarget)
+      console.log(context.checked)
+      console.log('----')
+
+      if (target.checked && !context.checked) {
+        console.log('Chage')
+        context.checked = true
+        // onChangeSkill(prevTarget, undefined)
+      }
+    }
 
     return {
       swiperComponent,
       user,
+      skills,
+      onChangeSkill,
+      specialists,
+      frameworks,
       nextSlide,
       prevSlide,
       toSlide,
