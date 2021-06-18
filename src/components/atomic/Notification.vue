91c5f2cc377eb9480c6a668303f54e1df6410d77
Alexander Katkov <katkov.alexander@yahoo.com>
2021-06-18 13:22:19 +0300

    feat(Input.vue): awesome input fix(Notification.vue): reduce js section

--- src/components/atomic/Notification.vue
+++ src/components/atomic/Notification.vue
@@ -3,12 +3,16 @@
     ref="info"
     v-show="show"
     :class="[
-      'flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-md bg-opacity-10',
+      'flex justify-center items-center font-medium m-1 py-1 px-2 bg-white rounded-md bg-opacity-10',
       `border-${type} text-${type} bg-${type}`,
     ]"
   >
     <div v-if="icon" class="mr-4">
       <component :is="icon" />
+      <InformationIcon v-if="type === 'info'" />
+      <CheckCircleIcon v-else-if="type === 'success'" />
+      <WarningTriangleIcon v-else-if="type === 'warning'" />
+      <EmojiSadIcon v-else-if="type === 'danger'" />
     </div>
     <div class="text-xl font-normal max-w-full text-yellow-700 flex-initial">
       <div class="py-2">
@@ -24,58 +28,22 @@
 <script>
 import { defineComponent, ref, watch, watchEffect } from 'vue'
 
-import {
-  InformationIcon,
-  WarningTriangleIcon,
-  CheckCircleIcon,
-  EmojiSadIcon,
-} from '@iconicicons/vue3'
 export default defineComponent({
   name: 'VNotification',
   props: {
-    show: {
-      type: Boolean,
-      default: true,
-    },
     type: {
       type: String,
       default: 'info',
       validate: (v) => ['info', 'success', 'warning', 'danger'].includes(v),
     },
-    message: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      default: undefined,
-    },
+    show: { type: Boolean, default: true },
+    message: { type: String, required: true },
+    description: { type: String, default: undefined },
   },
   setup(props) {
     const info = ref()
-    const icon = ref()
-
-    switch (props.type) {
-      case 'warning': {
-        icon.value = WarningTriangleIcon
-        break
-      }
-      case 'info': {
-        icon.value = InformationIcon
-        break
-      }
-      case 'success': {
-        icon.value = CheckCircleIcon
-        break
-      }
-      case 'danger': {
-        icon.value = EmojiSadIcon
-        break
-      }
-    }
 
     const highlight = () => {
-      console.log('as')
       const classes = ['bg-opacity-50']
       info.value.style.transition = '50ms ease'
       info.value.classList.add(...classes)
@@ -86,15 +54,11 @@ export default defineComponent({
     }
 
     watch(() => props.show, highlight)
-
     watchEffect(highlight, {
       flush: 'post',
     })
 
-    return {
-      info,
-      icon,
-    }
+    return { info }
   },
 })
 </script>
