d6b693207a10640bb0893ead2cec1a3a167922c1
alexander <33001830+7architect@users.noreply.github.com>
2021-07-30 01:55:38 +0300

    feat(#517): checkbox redesign

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import ViteFonts from 'vite-plugin-fonts'
 import ViteComponents from 'vite-plugin-components'
 import viteI18n from '@intlify/vite-plugin-vue-i18n'
 import ViteWindiCSS from 'vite-plugin-windicss'
 import ViteSvgIcons from 'vite-plugin-svg-icons'
 import path from 'path'
-import process from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,18 +27,6 @@ export default defineConfig({
             path: '@iconicicons/vue3',
           },
       ],
-    }),
-    ViteFonts({
-      google: {
-        display: 'swap',
-        families: [
-          {
-            name: 'Rubik',
-            styles:
-              'ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900',
-          },
-        ],
-      },
     }),
     viteI18n({
       include: './src/locales/**',
